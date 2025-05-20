c837fe13ce2414e54642ab2655427664c3e44d85
darmesdias <darmesdias@cofermeta.com.br>
2025-05-20 17:35:35 -0300

    implementando controller pra endpoints de processamento de remessas

---
+++ src/main/java/br/com/darmesdev/DesafioInter/DarmesGlobalAccCaseTecnico/controller/RemittanceController.java
@@ -0,0 +1,30 @@
+package br.com.darmesdev.DesafioInter.DarmesGlobalAccCaseTecnico.controller;
+
+import br.com.darmesdev.DesafioInter.DarmesGlobalAccCaseTecnico.model.dto.RemittanceRequest;
+import br.com.darmesdev.DesafioInter.DarmesGlobalAccCaseTecnico.model.entities.Transaction;
+import br.com.darmesdev.DesafioInter.DarmesGlobalAccCaseTecnico.service.RemittanceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/remittances")
+@RequiredArgsConstructor
+public class RemittanceController {
+    @Autowired
+    private final RemittanceService remittanceService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createRemittance(@RequestBody RemittanceRequest request) {
+        Transaction transaction = remittanceService.executeRemittance(
+                request.senderId(),
+                request.receiverId(),
+                request.amountBRL()
+        );
+        return ResponseEntity.ok(transaction);
+    }
+}
